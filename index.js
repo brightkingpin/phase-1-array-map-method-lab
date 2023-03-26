28fcb01f787fb7bc6b6df93f994e4c19f7a6f6ec
brightkingpin <luvandabright781@gmail.com>
2023-03-27 00:53:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    "the test driven development workflow",
+    "what is NaN and how can we check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?"
+  ];
 
-const titleCased = () => {
+  /*const titleCased = () => {
   return tutorials
+}*/
+
+  const titleCasedTutorials = tutorials.map(function (tutorial) {
+    const words = tutorial.split(' ');
+    const capitalizedWords = words.map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
+
+  return titleCasedTutorials;
 }
